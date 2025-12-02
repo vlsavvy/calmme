f833988dd50952d2d6f16a8783736401304cac80
Vidhyalakshmi B <b.vidhyalakshmi@live.co.uk>
2025-12-02 16:05:03 +0530

    Adding code for calm me

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def calmme_get():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("mcp_server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
